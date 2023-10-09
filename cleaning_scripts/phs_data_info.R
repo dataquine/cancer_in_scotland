dcb8f57f21fd0db8dea592bd301dcc045fbfd4bf
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-09 19:11:40 +0100

    Minor formatting

--- cleaning_scripts/phs_data_info.R
+++ cleaning_scripts/phs_data_info.R
@@ -4,6 +4,7 @@
 # Author: Lesley Duff
 # Description:
 #   URLs and file paths for data downloads from Public Health Scotland
+#
 
 phs_screening_bowel_url <- "https://publichealthscotland.scot/media/17689/2023-02-21-bowel-screening-kpi-report.xlsx"
 phs_screening_bowel_raw_filename <- "phs/screening/bowel/2023-02-21-bowel-screening-kpi-report.xlsx"
