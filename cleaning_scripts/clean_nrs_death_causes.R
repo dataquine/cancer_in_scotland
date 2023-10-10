caa0280007a001b33e7d39cab37012abea1e9a6f
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-10 13:31:15 +0100

    update

--- cleaning_scripts/clean_nrs_death_causes.R
+++ cleaning_scripts/clean_nrs_death_causes.R
@@ -6,14 +6,15 @@
 #   Clean National Records Scotland datasets for causes of death
 #
 # Source:
-#   Data:T able 1: All ages age-standardised death rates for all causes and certain selected causes, Scotland, 1994 to 2022
-# https://www.nrscotland.gov.uk/files//statistics/age-standardised-death-rates-esp/2022/age-standard-death-rates-22-tab1.xlsx
+# https://www.nrscotland.gov.uk/files//statistics/vital-events-ref-tables/2022/vital-events-22-ref-tabs-6.zip
+# Table 6.01 Deaths, by sex and cause, Scotland, 2009 to 2022
 
 # Background:
-# Age-standardised Death Rates Calculated Using the European Standard Population
-#https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/deaths/age-standardised-death-rates-calculated-using-the-esp
+# https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/general-publications/vital-events-reference-tables/2022/list-of-data-tables#section6
+
+#library(readxl) # For processing Excel spreadsheets
 
 
-library(readxl) # For processing Excel spreadsheets
+#source(here::here("R/nrs_data_info.R"))
 
 
