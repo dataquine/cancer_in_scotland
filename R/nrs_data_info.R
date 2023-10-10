f15f805921123095260d107901dc9c0e6926116d
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-10 18:27:44 +0100

    paths for NRS files

--- R/nrs_data_info.R
+++ R/nrs_data_info.R
@@ -6,13 +6,23 @@
 #   URLs and file paths for data downloads from National Records of Scotland
 #
 
-# Table 6.01 Deaths, by sex and cause, Scotland, 2009 to 2022
-
-#phs_screening_bowel_url <- "https://publichealthscotland.scot/media/17689/2023-02-21-bowel-screening-kpi-report.xlsx"
-nrs_death_cause_raw_filepath <- "nrs/death/vital-events-22-ref-tabs-6_6.01.csv"
+# Age-standardised Death Rates Calculated Using the European Standard Population
+# Table 1: All ages age-standardised death rates for all causes and certain selected causes, Scotland, 1994 to 2022
+# https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/deaths/age-standardised-death-rates-calculated-using-the-esp
+nrs_death_cause_url <- "https://www.nrscotland.gov.uk/files//statistics/age-standardised-death-rates-esp/2022/age-standard-death-rates-22-tab1.xlsx"
+nrs_death_cause_raw_filepath <- "nrs/death/age-standard-death-rates-22-tab1.xlsx"
 nrs_death_cause_filename <- "nrs_death_cause.csv"
 nrs_death_cause_filepath <- paste0("nrs/death/", nrs_death_cause_filename)
 # CSV text data for humans
 
-death_cause_shiny_filepath <- "shiny_app/data/nrs_death_cause.RDS"
+nrs_death_cause_shiny_filepath <- "shiny_app/data/nrs_death_cause.RDS"
 # Binary data for use by the shiny app
+
+# Spreadsheet info
+# N.B. This is awkwardly split in the sheet for no apparent reason
+# Decided to manually determine ranges of two jables then combine them
+nrs_death_cause_sheet <- "data for chart"
+nrs_death_cause_range <- "A4:N34"
+
+# There are confidence intervals not sure about how to use
+#nrs_death_cause_range2 <- "A78:O150"
