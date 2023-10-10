e1b24e9d18f890f05e4a4baaedf95301db8fbd41
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-11 00:41:03 +0100

    Plots for cause of death

--- R/death.R
+++ R/death.R
@@ -7,45 +7,50 @@
 #
 
 library(ggplot2)
+library(scales)
 
-death_all_causes <- "All causes"
+death_all_cause_value <- "All causes"
+nrs_caption <- "Data: National Records of Scotland"
 
-get_death_all_causes = function(df){
-  death_all_causes <- df %>%
-    filter(cause == death_all_causes)
-  
-  return (death_all_causes)
+plot_death_cause_title <- "Death rates for all causes"
+plot_death_cause_subtitle <- "Scotland. Age-standardised"
+plot_death_cause_xaxis_title <- "Year"
+plot_death_cause_yaxis_title <- "Rate"
+
+get_death_all_cause <- function(df) {
+  death_all_cause <- df %>%
+    filter(cause == death_all_cause_value)
+
+  return(death_all_cause)
 }
 
-get_death_selected_causes = function(df){
-  death_selected_causes <- df %>%
-    filter(cause == death_all_causes)
-  
-  return (death_selected_causes)
+get_death_selected_cause <- function(df) {
+  death_selected_cause <- df %>%
+    filter(cause != death_all_cause_value)
+
+  return(death_selected_cause)
 }
 
-#ndividual_causes <- death_cause %>%
-#  filter(cause != death_all_causes)
-
-
-plot_causes <- function(df) {
-  df %>%
+plot_death_cause <- function(df, plot_title = plot_death_cause_title,
+                             plot_subtitle = plot_death_cause_subtitle,
+                             plot_xaxis_title = plot_death_cause_xaxis_title,
+                             plot_yaxis_title = plot_death_cause_yaxis_title) {
+  plot <- df %>%
     ggplot(aes(x = year, y = rate)) +
     geom_point(color = "darkorchid4") +
     #   geom_bar(aes(fill = cause), stat = "identity") +
-    #geom_col(aes(fill = cause)) +
+    # geom_col(aes(fill = cause)) +
     # facet_wrap(~year) +
-    geom_line(aes(x = year, y = rate, colour=cause), group =1)+
-    
+    geom_line(aes(x = year, y = rate, colour = cause), group = 1, na.rm = TRUE) +
+    scale_y_continuous(label = scales::label_comma(scale = 1)) +
+    scale_x_continuous(breaks = seq(from = 1990, to = 2025, by = 5)) +
     labs(
-      title = "Age-standardised death rates",
-      subtitle = " All causes and certain selected causes, Scotland, 1994 to 2022",
-      y = "Rate",
-      x = "Year",
-      caption = "National Records of Scotland"
+      title = plot_title,
+      subtitle = plot_subtitle,
+      y = plot_yaxis_title,
+      x = plot_xaxis_title,
+      caption = nrs_caption
     ) +
     theme_bw(base_size = 15)
+  return(plot)
 }
-#all_causes
-
-#individual_causes
