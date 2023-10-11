cd63c38b497ffd746cd59cc88c15e4820e21d193
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-11 01:15:41 +0100

    trying a stacked bar chart with proportions - gives a better idea of which causes becoming more of an issue

--- R/death.R
+++ R/death.R
@@ -15,7 +15,7 @@ nrs_caption <- "Data: National Records of Scotland"
 plot_death_cause_title <- "Death rates for all causes"
 plot_death_cause_subtitle <- "Scotland. Age-standardised"
 plot_death_cause_xaxis_title <- "Year"
-plot_death_cause_yaxis_title <- "Rate"
+plot_death_cause_yaxis_title <- "Rate (proporton)"
 
 get_death_all_cause <- function(df) {
   death_all_cause <- df %>%
@@ -36,12 +36,14 @@ plot_death_cause <- function(df, plot_title = plot_death_cause_title,
                              plot_xaxis_title = plot_death_cause_xaxis_title,
                              plot_yaxis_title = plot_death_cause_yaxis_title) {
   plot <- df %>%
+    mutate(cause = str_trunc(cause, 15)) %>% 
     ggplot(aes(x = year, y = rate)) +
-    geom_point(color = "darkorchid4") +
+    geom_col(aes(fill = cause), na.rm = TRUE, position = "fill")+
+    #geom_point(color = "darkorchid4", na.rm = TRUE) +
     #   geom_bar(aes(fill = cause), stat = "identity") +
     # geom_col(aes(fill = cause)) +
-    # facet_wrap(~year) +
-    geom_line(aes(x = year, y = rate, colour = cause), group = 1, na.rm = TRUE) +
+    #facet_wrap(~year) +
+    #geom_line(aes(x = year, y = rate, colour = cause), group = 1, na.rm = TRUE) +
     scale_y_continuous(label = scales::label_comma(scale = 1)) +
     scale_x_continuous(breaks = seq(from = 1990, to = 2025, by = 5)) +
     labs(
@@ -51,6 +53,7 @@ plot_death_cause <- function(df, plot_title = plot_death_cause_title,
       x = plot_xaxis_title,
       caption = nrs_caption
     ) +
-    theme_bw(base_size = 15)
+    #theme_bw(base_size = 15)
+    theme(legend.position="bottom")
   return(plot)
 }
