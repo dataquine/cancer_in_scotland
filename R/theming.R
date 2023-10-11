980981d1cd358df10d6d4c834547673fa8516319
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-11 21:07:27 +0100

    For storing anything relating to user interface e.g. colours, fonts, themes

---
+++ R/theming.R
@@ -0,0 +1,10 @@
+#
+# File: theming.R
+# Date: 2023-10-11
+# Author: Lesley Duff
+# Description:
+#   User interface colours, fonts, theming etc
+#
+
+# How about a colour palette from cancer ribbons?
+#https://www.medicalnewstoday.com/articles/323448#all-cancers
