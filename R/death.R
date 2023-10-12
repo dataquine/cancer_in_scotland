82144bf4a26039af89f7a9ed63519ba5d90bf4da
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-12 01:22:43 +0100

    death rate

--- R/death.R
+++ R/death.R
@@ -9,39 +9,53 @@
 library(ggplot2)
 library(scales)
 
-death_all_cause_value <- "All causes"
-nrs_caption <- "Data: National Records of Scotland"
+# Attribution to NRS for the data source
+plot_source_nrs <- "Source: National Records of Scotland"
 
-plot_death_cause_title <- "Death rates for all causes"
-plot_death_cause_subtitle <- "Scotland. Age-standardised"
-plot_death_cause_xaxis_title <- "Year"
-plot_death_cause_yaxis_title <- "Rate (proporton)"
+# death_all_cause_value <- "All causes"
+death_all_cause_simple_value <- "All causes"
+# nrs_caption <- "Data: National Records of Scotland"
 
-get_death_all_cause <- function(df) {
-  death_all_cause <- df %>%
-    filter(cause == death_all_cause_value)
 
-  return(death_all_cause)
+# Plot: cancer death against death from all causes
+plot_death_cause_simple_title <- "Death rate from cancer as percentage all causes"
+plot_death_cause_simple_subtitle <- ""
+plot_death_cause_simple_caption <- plot_source_nrs
+
+# Plot: cancer death against all causes
+plot_death_cause_simple_all_causes_title <- "Death rate from cancer against all causes"
+plot_death_cause_simple_all_causes_subtitle <- "Scotland. Age-standardised"
+plot_death_cause_simple_all_causes_caption <- plot_source_nrs
+plot_death_cause_simple_all_causes_xaxis_title <- "Year"
+plot_death_cause_simple_all_causes_yaxis_title <- "Rate (proportion)"
+
+
+get_death_all_cause_simple <- function(df) {
+  death_all_cause_simple <- df %>%
+    filter(cause_simple == death_all_cause_simple_value)
+
+  return(death_all_cause_simple)
 }
 
-get_death_selected_cause <- function(df) {
-  death_selected_cause <- df %>%
-    filter(cause != death_all_cause_value)
+get_death_selected_cause_simple <- function(df) {
+  death_selected_cause_simple <- df %>%
+    filter(cause_simple != death_all_cause_simple_value)
 
-  return(death_selected_cause)
+  return(death_selected_cause_simple)
 }
 
-plot_death_cause <- function(df, plot_title = plot_death_cause_title,
-                             plot_subtitle = plot_death_cause_subtitle,
-                             plot_xaxis_title = plot_death_cause_xaxis_title,
-                             plot_yaxis_title = plot_death_cause_yaxis_title,
-                             plot_year_start = 1994,
-                             plot_year_end = 2022,
-                             plot_year_by = 2) {
+plot_death_cause_simple <- function(df, plot_title = plot_death_cause_simple_all_causes_title,
+                                    plot_subtitle = plot_death_cause_simple_all_causes_subtitle,
+                                    plot_caption = plot_death_cause_simple_all_causes_caption,
+                                    plot_xaxis_title = plot_death_cause_simple_all_causes_xaxis_title,
+                                    plot_yaxis_title = plot_death_cause_simple_all_causes_yaxis_title,
+                                    plot_year_start = 1994,
+                                    plot_year_end = 2022,
+                                    plot_year_by = 2) {
   plot <- df %>%
-    mutate(cause = str_trunc(cause, 20)) %>%
+    #mutate(cause = str_trunc(cause, 20)) %>%
     ggplot(aes(x = year, y = rate)) +
-    geom_col(aes(fill = cause), na.rm = TRUE, position = "fill") +
+    geom_col(aes(fill = cause_simple),  position = "fill") +
     # geom_point(color = "darkorchid4", na.rm = TRUE) +
     #   geom_bar(aes(fill = cause), stat = "identity") +
     # geom_col(aes(fill = cause)) +
@@ -56,9 +70,10 @@ plot_death_cause <- function(df, plot_title = plot_death_cause_title,
     labs(
       title = plot_title,
       subtitle = plot_subtitle,
+      caption = plot_caption,
       y = plot_yaxis_title,
       x = plot_xaxis_title,
-      caption = nrs_caption
+      fill = ""
     ) +
     # theme_bw(base_size = 15)
     theme(legend.position = "bottom")
@@ -77,25 +92,30 @@ get_death_by_year_cause_simple <- function(df, filter_cause_simple, filter_year)
 }
 
 # Death rate from cancer pie chart ####
-plot_death_rate_cancer <- function(df) {
+plot_death_rate_cancer <- function(df, plot_title = plot_death_cause_simple_title,
+                                   plot_subtitle = plot_death_cause_simple_subtitle,
+                                   plot_caption = plot_death_cause_simple_caption) {
   plot <- df %>%
     # Basic piechart with percentage labels
     ggplot(aes(x = "", y = percent, fill = category)) +
-    geom_col(color = "white", width = 1) +
+  #  geom_col(color = "white", width = 10) +
+    geom_col() +
     geom_label(aes(label = percent_labels),
       position = position_stack(vjust = 0.5),
       show.legend = FALSE # hide the 'a' symbol
     ) +
     #   scale_fill_manual(values = c("red", "yellow")) +
-    coord_polar(theta = "y") + # ?coord_polar
+   coord_polar(theta = "y") + # ?coord_polar
     theme(
       axis.text.x = element_blank(), # remove 25/50 etc
       axis.title = element_blank(), # remove percent
+      axis.ticks = ggplot2::element_blank(),
       panel.grid = element_blank() # remove gridlines
-      )
-  
+    ) +
     labs(
-      title = "Death rate from cancer as percentage all causes",
+      title = plot_title,
+      subtitle = plot_subtitle,
+      caption = plot_caption,
       x = "",
       y = "Percent",
       fill = ""
