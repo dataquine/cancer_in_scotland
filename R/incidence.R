1dda94d44cc48e33f2f0e040f3b5bc15e36e285a
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-15 22:20:52 +0100

    Start adding male/female lookup for abbreviations

--- R/incidence.R
+++ R/incidence.R
@@ -12,8 +12,15 @@ library(gt)
 # library(ggrepel)
 
 # Plot
+# Plot 1 Linechart 
 cancer_incidence_plot_filepath <- "images/plot/plot_cancer_incidence.png"
-cancer_incidences_by_year <- "Number of cancer incidences by year in Scotland"
+cancer_incidences_by_year <- "Number of Cancer Incidences by Year and Sex"
+
+# Plot 2 Box plot
+cancer_incidence_plot_box_plot_filepath <- "images/plot/plot_cancer_incidence_boxplot.png"
+#cancer_incidences_by_year <- "Number of Cancer Incidences by Year and Sex"
+
+# Plot titles
 
 # These are variables in the dataframe
 incidence_cancer_site_all_value <- "All cancer types"
@@ -21,6 +28,10 @@ incidence_cancer_site_value <- "All"
 incidence_cancer_site_all_ages_value <- "All ages"
 
 incidence_cancer_site_icd10code_all <- "C00-C97, excluding C44"
+
+incidence_all_sex_filter <- c("All", "Females", "Male")
+incidence_sex_label <- "Sex"
+incidence_incidences_label <- "Incidences" # Used for axis
 
 
 # In case the raw data changes in the future lets build in some assertive
@@ -33,7 +44,7 @@ check_valid_icd10 <- function(df) {
     # Check that we are not including non-malignant melanoma (C44)
     verify(!str_detect(cancer_site_icd10code, "C44")) %>%
     # check our ICD codes contain a 'C' code
-    verify(str_detect(cancer_site_icd10code, "C")) %>% 
+    verify(str_detect(cancer_site_icd10code, "C")) %>%
     # We assume after these checks that it is safe to use
     return(TRUE)
   return(FALSE)
@@ -45,26 +56,34 @@ plot_incidences_by_year <- function(df,
                                     show_covid = TRUE,
                                     plot_title = cancer_incidences_by_year,
                                     plot_x = "",
-                                    plot_y = "Incidences\n") {
+                                    plot_y = "Incidences\n",
+                                    filter_sex = incidence_all_sex_filter,
+                                    plot_sex = incidence_sex_label) {
   plot <- df %>%
     mutate(label = if_else(year == max(year) |
       year == min(year),
     as.character(incidences_age), NA_character_
     )) %>%
-    ggplot(aes(x = year, y = incidences_age)) +
-    geom_line() +
+    filter(sex %in% filter_sex) %>%
+    ggplot(aes(x = year, y = incidences_age, color = sex)) +
+    geom_line(linewidth = 1) +
+
+    # Too messy when all male and female
+    # geom_smooth(se = FALSE, method = lm, alpha = 0.3) +
+
     #    geom_label_repel(aes(label = label), # ?geom_label_repel
     # nudge_x = 0,
     #     nudge_y = Inf,
     #   na.rm = TRUE
     #    ) +
-    geom_hline(
-      yintercept = 0,
-      #   colour = "red",
-      alpha = 0.5,
-      linetype = 1
-    ) +
-    geom_point() +
+    # geom_hline(
+    #   yintercept = Inf,
+    #   #   colour = "red",
+    #   alpha = 0.5,
+    #   linetype = 1
+    # ) +
+    geom_point(size = 3) +
+    scale_colour_cis_qualitative(palette_sex) +
     scale_y_continuous(label = scales::label_comma()) +
     scale_x_continuous(breaks = seq(
       from = min(df$year),
@@ -80,11 +99,12 @@ plot_incidences_by_year <- function(df,
       subtitle = paste(min(df$year), max(df$year), sep = "-"),
       x = plot_x,
       y = plot_y,
-      caption = source_phs
+      caption = source_phs,
+      colour = plot_sex
     )
   if (show_covid) {
     plot <- plot + geom_vline(
-      xintercept = 2020,
+      xintercept = 2020, # The year it started
       colour = "red",
       alpha = 0.7,
       linetype = 3
@@ -139,14 +159,15 @@ get_incidences_by_year <- function(df) {
     # Only interested in top level numbers
     filter(cancer_site == "All cancer types") %>%
     # Only interested in all sexes
-    filter(sex == "All") %>%
+    # TODO GENDERS
+    # filter(sex == "All") %>%
     # Only interested in all age ranges
     filter(incidences_age_range == "All ages") %>%
     # select(year, cancer_site,sex,incidences_age_range) %>%
     group_by(year, cancer_site, sex, incidences_age_range, incidences_age) %>%
     summarise() %>%
     ungroup() %>%
-    select(year, incidences_age)
+    select(year, incidences_age, sex)
 }
 
 # For every year break down each cancer type by number of instances
@@ -159,10 +180,69 @@ get_incidences_by_cancer_site <- function(df) {
     filter(sex == incidence_cancer_site_value) %>%
     # Only interested in all age ranges
     filter(incidences_age_range == incidence_cancer_site_all_ages_value) %>%
-    group_by(year, cancer_site, sex, incidences_age_range, incidences_age) %>%
-    summarise() %>%
+    group_by(
+      year, cancer_site, cancer_site_short, sex,
+      incidences_age_range, incidences_age
+    ) %>%
+    summarise(
+      total_incidences = sum(incidences_age),
+      avg_incidences = mean(incidences_age)
+    ) %>%
+    #  summarise(total_incidences = incidences_age) %>%
     # filter(year==1997) %>%
     ungroup() %>%
-    select(year, cancer_site, incidences_age)
+    select(
+      year, cancer_site, cancer_site_short, incidences_age,
+      avg_incidences, total_incidences
+    )
   return(incidences_by_cancer_site)
+}
+
+simplify_cancer_site <- function(df) {
+  # These are for display purposes only to help long medical names fit on screen
+  simplified <- df %>%
+    mutate(cancer_site_short = case_match(cancer_site,
+      "Acute lymphoblastic leukaemia" ~ "ALL...",
+      "Acute myeloid leukaemia" ~ "AML",
+      "Bone and articular cartilage" ~ "Bone & A...",
+      "Bone and connective tissue" ~ "Bone & C...",
+      "Malignant brain cancer" ~ "Brain",
+      "All brain and CNS tumours (malignant and non-malignant)" ~
+        "Brain & CNS...",
+      "Malig brain ca (incl pit. gland, cranio. duct, pineal gland)" ~
+        "Brain incl...",
+      "Chronic lymphocytic leukaemia" ~ "CLL...",
+      "Chronic myeloid leukaemia" ~ "CML...",
+      "Colorectal cancer" ~ "Colorectal...",
+      "Connective tissue" ~ "Connective...",
+      "Head and neck" ~ "Head...",
+      "Hodgkin lymphoma" ~ "Hodgkin...",
+      "Lip, oral cavity and pharynx" ~ "Lip/Oral...",
+      "Liver and intrahepatic bile ducts" ~ "Liver & bile...",
+      "Trachea, bronchus and lung" ~ "Lung...",
+      "Malignant melanoma of the skin" ~ "Skin...",
+      "Multiple myeloma and malignant plasma cell neoplasms" ~ "Myeloma...",
+      "Mouth (IARC definition)" ~ "Mouth...",
+      "Non-Hodgkin lymphoma" ~ "Non-Hodgkin...",
+      "Oropharyngeal cancers" ~ "Oropharyngeal...",
+      "Rectum and rectosigmoid junction" ~ "Rectum...",
+      "Salivary glands" ~ "Salivary...",
+      .default = cancer_site
+    ))
+  return(simplified)
+}
+
+plot_incidences_by_year_boxplot <- function(df, 
+                                            plot_x = incidence_incidences_label,
+                                            plot_fill = incidence_sex_label) {
+  plot <- df %>%
+    ggplot(aes(x = incidences_age,fill = sex)) +
+    geom_boxplot(color="black") +
+    scale_fill_cis_qualitative(palette_sex) +
+    scale_x_continuous(label = scales::label_comma()) +
+    labs(
+      x = paste0("\n", plot_x),
+      fill = plot_fill
+    )
+  return(plot)
 }
