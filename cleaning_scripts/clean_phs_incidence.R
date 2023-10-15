8a9910e0ba912140b44a1c20505588ad49a89a49
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-15 04:08:58 +0100

    Clean ICD-10 standards

--- cleaning_scripts/clean_phs_incidence.R
+++ cleaning_scripts/clean_phs_incidence.R
@@ -82,10 +82,11 @@ incidence_rate_age_ninety_andover_to <- "90 and over"
 incidence_columns_to_keep <- c(
   "cancer_site", "sex", "year",
   "incidences_age_range", "incidences_age",
-  "incidence_rate_age_range", "incidence_rate_age"
+  "incidence_rate_age_range", "incidence_rate_age",
+  "cancer_site_icd10code"
 )
 
-incidence_clean <- incidence_raw %>%
+incidence_unfiltered <- incidence_raw %>%
   # incidences_age columns
   pivot_longer(
     cols =
@@ -122,9 +123,68 @@ incidence_clean <- incidence_raw %>%
     .default = incidence_rate_age_range
   )) %>%
   # Choose columns to keep
-  select(all_of(incidence_columns_to_keep))
+  select(all_of(incidence_columns_to_keep)) %>% 
+  relocate(cancer_site_icd10code, .after = cancer_site)
 
-# names(incidence_clean)
+# We need to our analysis in line with the official statistics
+# In particular in the original spreadsheet has the following definition
+# 'All cancer types' = 'C00-C97, excluding C44'
+# We need to exclude some ICD-10 codes
+# What ICD-10 codes are present?
+icd10_cancers <- incidence_unfiltered %>%
+  select(cancer_site, cancer_site_icd10code) %>%
+  group_by(cancer_site_icd10code, cancer_site) %>%
+  arrange(cancer_site_icd10code) %>%
+  summarise() %>%
+  ungroup()
+
+icd10_cancers
+
+# From this list we find that there are records for C44 so we need to remove
+# them there are also ones with a 'D' code rather than 'C'
+
+icd10_cancers_filtered <- icd10_cancers %>%
+  # For filtering we are looking at things not already categorised as
+  # 'All cancer types' ie different cancer types
+  # Warning! C44 appear inside the cancer_site_icd10code for
+  # 'C00-C97, excluding C44'
+  # incidence_cancer_site_icd10code_all
+  # We need to keep All cancer types in the dataset!
+  filter(
+    cancer_site == "All cancer types" |
+      (!str_detect(cancer_site_icd10code, "C44") &
+         str_detect(cancer_site_icd10code, "C")
+      )
+  )
+
+# To exclude include Non-melanoma in this dataset include 3 codes:
+# Unsure about the ones that *also* contain 'M' codes?
+# C44	Non-melanoma skin cancer
+# C44, M-8050-8078, M-8083-8084	Squamous cell carcinoma of the skin
+# C44, M-8090-8098	Basal cell carcinoma of the skin
+
+# Similarly we have All brain and CNS tumours (malignant and non-malignant)
+# C70-C72, C75.1-C75.3, D18.0, D32-D33, D35.2-D35.4, D42-D43, D44.3-D44.5
+# As this also contains C code will keep until we uncover a problem doing so
+
+# Our highest C code seem to be 'C92.1-C92.2' which is below C97
+
+# We have rows with non-C codes
+# D05 Carcinoma in situ of the breast
+# D06	Carcinoma in situ of the cervix uteri
+# D18.0, D32-D33, D35.2-D35.4, D42-D43, D44.3-D44.5 Non-malig brain ca (incl pit.gland,cranio.duct,pineal gland)
+
+# Now we have a frame of what to keep remove the rest from our analysis set
+incidence_clean <- incidence_unfiltered %>%
+  # We want to keep everything in our existing dataset that has a match in
+  # the filtered cancers but to lose things that dont match
+  #  This seems like an inncer join would be appropriate
+  # Keeps observations from incidence_all that have a matching key in
+  # icd10_cancers_filtered.
+  inner_join(icd10_cancers_filtered)
+#rm(incidence_clean)
+
+names(incidence_clean)
 # View(incidence_clean)
 
 # Write our clean version of death cause data ----
@@ -142,6 +202,7 @@ saveRDS(incidence_clean, file = here::here(
 ))
 
 rm(list = ls(pattern = "column_"))
+rm(list = ls(pattern = "icd10_cancers"))
 rm(list = ls(pattern = "incidence_"))
 rm(list = ls(pattern = "incidences_"))
 rm(list = ls(pattern = "phs_"))
