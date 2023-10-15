08816f05a1d2a19c120f8679d7c8fed6e7d1e261
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-15 22:20:16 +0100

    Generic functions for plotting etc

---
+++ R/cancer_in_scotland.R
@@ -0,0 +1,23 @@
+#
+# File: cancer_in_scotland.R
+# Date: 2023-10-15
+# Author: Lesley Duff
+# Description:
+#   Project-wide variables/functions
+#
+
+library(here)
+
+enable_plot_saving <- TRUE
+
+do_cis_plot <- function(cis_plot,
+                    save_path = "",
+                    save_plot = enable_plot_saving) {
+  print(cis_plot)
+  if (save_plot) {
+    ggsave(
+      here::here(save_path),
+      cis_plot
+    )
+  }
+}
