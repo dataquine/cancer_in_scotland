5a2dcf2a92432714048c8fcef12bf92ebfc40900
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-19 02:38:32 +0100

    Added waiting times

--- R/phs_data_info.R
+++ R/phs_data_info.R
@@ -24,6 +24,24 @@ phs_incidence_filename <- "phs_incidence.csv"
 phs_incidence_filepath <- paste0("phs/incidence/", phs_incidence_filename)
 # CSV text data for humans
 
+# Waiting Times
+## 31 Day Standard
+phs_waiting_times_31_days_url <- "https://www.opendata.nhs.scot/dataset/11c61a02-205b-43f6-9297-243679103617/resource/58527343-a930-4058-bf9e-3c6e5cb04010/download/cwt_31_day_standard.csv"
+phs_waiting_times_31_days_raw_filepath <- "phs/waiting_times/cwt_31_day_standard.csv"
+phs_waiting_times_31_days_filename <- "phs_waiting_time_31_day_standards.csv"
+phs_waiting_times_31_days_filepath <- paste0("phs/waiting_times/", phs_waiting_times_31_days_filename)
+
+## 62 Day Standard
+phs_waiting_times_62_days_url <- "https://www.opendata.nhs.scot/dataset/11c61a02-205b-43f6-9297-243679103617/resource/23b3bbf7-7a37-4f86-974b-6360d6748e08/download/cwt_62_day_standard.csv"
+phs_waiting_times_62_days_raw_filepath <- "phs/waiting_times/cwt_62_day_standard.csv"
+phs_waiting_times_62_days_filename <- "phs_waiting_time_62_day_standards.csv"
+phs_waiting_times_62_days_filepath <- paste0("phs/waiting_times/", phs_waiting_times_62_days_filename)
+
+# Combined file
+phs_waiting_times_days_filename <- "phs_waiting_time_day_standards.csv"
+phs_waiting_times_days_filepath <- paste0("phs/waiting_times/", phs_waiting_times_days_filename)
+
+
 phs_incidence_shiny_filepath <- "shiny_app/data/phs_incidence.RDS"
 # Binary data for use by the shiny app
 
