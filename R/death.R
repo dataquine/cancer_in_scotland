d9e4f7c1fde047c1a5bb9418c0a381efeeedbdce
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-10-20 09:14:00 +0100

    plotting

--- R/death.R
+++ R/death.R
@@ -22,11 +22,16 @@ plot_death_cause_simple_subtitle <- ""
 plot_death_cause_simple_caption <- plot_source_nrs
 
 # Plot: cancer death against all causes
-plot_death_cause_simple_all_causes_title <- "Death rate from cancer - all causes"
+plot_death_selected_cause_time_plot_filepath <- "images/plot/plot_death_selected_cause_time.png"
+plot_death_cause_simple_all_causes_title <- "Death rate - all causes"
 plot_death_cause_simple_all_causes_subtitle <- "Scotland. Age-standardised"
 plot_death_cause_simple_all_causes_caption <- plot_source_nrs
 plot_death_cause_simple_all_causes_xaxis_title <- "Year"
 plot_death_cause_simple_all_causes_yaxis_title <- "Death rate (proportion)"
+plot_death_cause_simple_all_causes_individual_yaxis_title <- "Death rate"
+plot_death_cause_simple_all_causes_individual_xaxis_title <- "Year (1994 - 2022)"
+plot_death_selected_cause_time_individual_plot_filepath <- "images/plot/plot_death_selected_cause_time_individual.png"
+
 
 plot_year_covid <- 2020
 
@@ -70,19 +75,13 @@ plot_death_cause_simple <- function(df, plot_title = plot_death_cause_simple_all
         fill = cause_simple,
         alpha = alpha_cancer
       ),
-      position = "fill", colour = "white"
-    ) + #
-    # scale_colour_manual(values = c("no" = "white", "yes" = "black"),
-    # scale_colour_manual(values = c("no" = "black", "yes" = "white"),
-    #                                           guide = "none") +
-    # scale_colour_cis_qualitative(values = c("no" = "white",
-    #                                        "yes" = "black"),
-    #                          guide = "none") +
+      position = "fill", 
+      colour = "white"
+    ) + 
     scale_alpha_manual(
       values = c("TRUE" = 1, "FALSE" = 0.8),
       guide = "none"
     ) +
-
     # Show a line where COVID-19 started
     geom_vline(
       xintercept = plot_year_covid,
@@ -107,8 +106,8 @@ plot_death_cause_simple <- function(df, plot_title = plot_death_cause_simple_all
       title = plot_title,
       subtitle = plot_subtitle,
       caption = plot_caption,
-      y = plot_yaxis_title,
-      x = plot_xaxis_title,
+      y = paste0(plot_yaxis_title, "\n"),
+      x = paste0("\n", plot_xaxis_title),
       fill = ""
     ) +
     # scale_fill_cis_sequential()+
@@ -122,6 +121,72 @@ plot_death_cause_simple <- function(df, plot_title = plot_death_cause_simple_all
   # theme_bw(base_size = 15)
   # theme(legend.position = "bottom")
   # View(plot_df)
+  return(plot)
+}
+
+# See each cause by itself
+plot_death_cause_simple_individual <- function(df, plot_title = plot_death_cause_simple_all_causes_title,
+                                    plot_subtitle = plot_death_cause_simple_all_causes_subtitle,
+                                    plot_caption = plot_death_cause_simple_all_causes_caption,
+                                    plot_xaxis_title = plot_death_cause_simple_all_causes_individual_xaxis_title,
+                                    plot_yaxis_title = plot_death_cause_simple_all_causes_individual_yaxis_title,
+                                    plot_year_start = 1994,
+                                    plot_year_end = 2022,
+                                    plot_year_by = 2) {
+  plot <- df %>%
+    mutate(
+      highlight = case_when(
+        cause_simple == death_cancer_cause_simple_value ~ "yes",
+        TRUE ~ "no"
+      ),
+      alpha_cancer = case_when(
+        cause_simple == death_cancer_cause_simple_value ~ TRUE,
+        TRUE ~ FALSE
+      )
+    ) %>%
+    #   ggplot(aes(x = year, y = rate, colour = highlight, alpha = alpha_cancer)) +
+    ggplot(aes(x = year, y = rate, alpha = alpha_cancer)) +
+    geom_area(fill = cis_colour_cancer, colour = cis_colour_cancer)+
+#    geom_col(
+#      aes(
+#        fill = cause_simple,
+#        alpha = alpha_cancer
+#      ),
+# #position = "fill", 
+#      #position = "dodge", 
+#      colour = "white"
+#    ) + 
+    facet_wrap(~cause_simple)+
+    scale_alpha_manual(
+      values = c("TRUE" = 1, "FALSE" = 0.8),
+      guide = "none"
+    ) +
+    # Show a line where COVID-19 started
+    geom_vline(
+      xintercept = plot_year_covid,
+      colour = "red",
+      alpha = 0.7,
+      linetype = 3
+    ) +
+    scale_y_continuous(label = scales::label_comma(scale = 1)) +
+    scale_x_continuous(breaks = seq(
+      from = plot_year_start,
+      to = plot_year_end,
+      by = plot_year_by
+    )) +
+    labs(
+      title = plot_title,
+      subtitle = plot_subtitle,
+      caption = plot_caption,
+      y = paste0(plot_yaxis_title, "\n"),
+      x = paste0("\n", plot_xaxis_title),
+      fill = ""
+    ) +
+    theme(
+      panel.grid.major.y = ggplot2::element_blank(),
+      axis.text.y = element_blank(),
+      axis.text.x=element_blank() #remove x axis labels
+      )
   return(plot)
 }
 
